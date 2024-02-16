1f26e85aeaffdc41829021fb213b45a86e8c8536
Benny1610-yes <benedict.newton10@gmail.com>
2024-02-16 17:03:08 +0000

    Re-Creating old Pacman Project from ground up.

---
+++ src/Old_Code/runner.java
@@ -0,0 +1,22 @@
+package Old_Code;
+
+import Old_Code.Map;
+
+import javax.swing.JFrame;
+
+public class runner {
+	
+	public static void main (String[] args)
+	{
+		Map bob= new Map();
+		bob.Loadmap("LvL1.txt");	
+		
+		
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setContentPane(bob);
+		window.setSize(1000,500);
+		window.setVisible(true);
+		
+	}
+}
